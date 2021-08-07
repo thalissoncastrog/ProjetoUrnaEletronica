e113fb9c3076c36d517951be5fc5f5b0876da16e
Thalisson Castro <adaothalisson@hotmail.com>
2021-08-07 05:13:10 -0300

    Criação do "Banco de dados" de candidatos

---
+++ etapas.js
@@ -0,0 +1,53 @@
+let stages = [
+    {
+        title: 'Vereador',
+        numbers: 5,
+        candidates: [
+            {
+                number: '38111',
+                name: 'Bacural',
+                party: 'ABC',
+                pictures: [
+                    { url: 'images/BACURAL_V.png', subtitle: 'Vereador' }
+                ]
+            },
+
+            {
+                number: '77222',
+                name: 'Boga',
+                party: 'DEFG',
+                pictures: [
+                    { url: 'images/BOGA_V.png', subtitle: 'Vereador' }
+                ]
+            }
+        ]
+    },
+
+    {
+        title: 'Prefeito',
+        numbers: 2,
+        candidates: [
+            {
+                number: '99',
+                name: 'Jorge Piu Piu',
+                party: 'ABC',
+                vice: 'Moranguinho da Habitação',
+                pictures: [
+                    { url: 'images/JORGE_PIUPIU_P.png', subtitle: 'Prefeito' },
+                    { url: 'images/MORANGUINHO_DA_HABITAÇÃO_VP.png', subtitle: 'Vice-Prefeito', small: true }
+                ]
+            },
+
+            {
+                number: '84',
+                name: 'Papai Noel',
+                party: 'QWERTY',
+                vice: 'Agnaldo',
+                pictures: [
+                    { url: 'images/PAPAI_NOEL_P.png', subtitle: 'Prefeito' },
+                    { url: 'images/AGNALDO_VP.png', subtitle: 'Vice-Prefeito', small: true }
+                ]
+            }
+        ]
+    }
+];
